ce85d71db84d75159b2429b9d3b56ea5758a5e7e
Peter Song <psong562@gmnail.com>
2021-12-22 20:10:55 -0800

    added commas

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -11,13 +11,16 @@ ProductTag.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     product_id: {
       type: DataTypes.INTEGER,
-      references: {model: "Product", key: "id"}
+      references: {model: "product", key: "id"},
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {model: "tag", key: "id"},
     }
-
   },
   {
     sequelize,
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 // import sequelize connection
-const { join } = require ('path')
 const {Category, Product, ProductTag, Tag} = require('./models')
 
 
@@ -13,7 +12,7 @@ app.use(require('./routes'))
 
 // sync sequelize models to the database, then turn on the server
 async function init() {
-  await require('./db').sync()
+  await require('./config/connection.js').sync()
   app.listen(process.env.PORT || 3000)
 }
 
