6f632c0613a61714ca5c37e76c8f8f14a8396cc2
Peter Song <psong562@gmnail.com>
2021-12-22 20:15:41 -0800

    updated

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,6 @@ require('dotenv').config()
 
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(`mysql:root:rootroot@localhost:3306/ecommerce_db`)
-
-
-  // process.env.JAWSDB_URL ? process.env.JAWSDB_URL : process.env.LOCALDB_URL
+const sequelize = new Sequelize(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : process.env.LOCALDB_URL)
 
 module.exports = sequelize
