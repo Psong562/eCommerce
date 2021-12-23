a180e2b75ff4aef70ab780ce35f2b1d6bf77c0f5
Peter Song <psong562@gmnail.com>
2021-12-22 20:14:35 -0800

    updated spelling

--- models/Product.js
+++ models/Product.js
@@ -29,7 +29,7 @@ Product.init(
         type: DataTypes.INTEGER,
         allowNull: false,
         defaultValue: 10,
-        validate : {isNumveric: true},
+        validate : {isNumeric: true},
     },
     category_id: {
         type: DataTypes.INTEGER,
